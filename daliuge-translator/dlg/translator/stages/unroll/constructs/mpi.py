c5c70c5912f7b5dd2ca4b88756eae76c7c479002
BaiChang Zhang <baichangzhang@BaiChanndiannao.mymodem>
2026-09-22 01:11:14 +0800

    Fix pylint warning in MPI handler

--- daliuge-translator/dlg/translator/stages/unroll/constructs/mpi.py
+++ daliuge-translator/dlg/translator/stages/unroll/constructs/mpi.py
@@ -7,4 +7,6 @@ class MPIHandler:
     construct_type = Categories.MPI
 
     def degree_of_parallelism(self, node: Any, ctx: Any) -> int:
+        del ctx
+
         return int(node.jd["num_of_procs"])
