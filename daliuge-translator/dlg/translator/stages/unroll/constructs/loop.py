3677687c82e8eb9fedd52bb5b570470b1e1fd367
BaiChang Zhang <baichangzhang@BaiChanndiannao.mymodem>
2026-09-22 00:54:36 +0800

    Add Loop degree of parallelism handler

---
+++ daliuge-translator/dlg/translator/stages/unroll/constructs/loop.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+from dlg.translator.errors import GInvalidNode
+from dlg.translator.vocabulary import Categories
+
+
+class LoopHandler:
+    construct_type = Categories.LOOP
+
+    def degree_of_parallelism(self, node: Any, ctx: Any) -> int:
+        for key in [
+            "num_of_iter",
+            "Number of Iterations",
+            "Number of loops",
+        ]:
+            if key in node.jd and node.jd[key]:
+                return int(node.jd[key])
+
+        raise GInvalidNode(
+            f"Loop '{node.name}' ({node.id}) has no iteration count. "
+            "One of 'num_of_iter', 'Number of Iterations', 'Number of loops' is required."
+        )
